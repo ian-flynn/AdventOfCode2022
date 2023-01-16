24ea193b77d329dc1ae55265231959454f412809
Ian Flynn <ianflynnwork@gmail.com>
2023-01-16 18:47:18 -0500

    solved

--- Day1/solution.js
+++ Day1/solution.js
@@ -1,7 +1,26 @@
 const fs = require('fs');
-const elfCalories = fs.readFileSync(`${__dirname}/index`)
+const elfCalories = fs.readFileSync(`${__dirname}/input`).toString().split('\n')
 
 console.log(elfCalories);
 
+const findElf = (arr) => {
+    let bigElf;
+    let bigSum = 0;
+    let sum = 0;
+    let place = 1;
+    for(let i=0; i<arr.length; i++){
+        if ( arr[i] === '' ){
+            sum = 0;
+            place++
+        } else {
+            sum += parseInt(arr[i])
+            if ( sum > bigSum ){
+                bigSum = sum;
+                bigElf = place;
+            } 
+        }
+    }
+    return `The ${bigElf}th elf is carring the most, with ${bigSum} calories`;
 
-console.log('Hello Day 1!');
+}
+console.log(findElf(elfCalories))
