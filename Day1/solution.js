0bff10e0bd3698c990814ae94b4ef3726c68a16f
Ian Flynn <ianflynnwork@gmail.com>
2023-01-16 19:29:45 -0500

    part two solved

--- Day1/solution.js
+++ Day1/solution.js
@@ -5,11 +5,15 @@ console.log(elfCalories);
 
 const findElf = (arr) => {
     let bigCals = 0, sum = 0;
-    const topThree = ['howdy'];
+    const topThree = [];
     arr.forEach(cal => {
         if(!cal){
             //check if top three is has less than 3 things in it, if so add sum to it
-            //if top three is full, 
+            if(topThree.length < 3) topThree.push(sum)
+            if(topThree.length === 3 && sum > Math.min(...topThree)){
+                topThree.sort((a,b) => a-b).shift()
+                topThree.push(sum)
+            }
             sum = 0;
 
         } else {
@@ -19,7 +23,8 @@ const findElf = (arr) => {
             }
         }
     })
-    return `The elf carring the most has ${bigCals} calories, the top three are ${topThree}`;
+    let total = topThree.reduce((acc,curr)=> acc+curr)
+    return `The elf carring the most has ${bigCals} calories, the top three are ${topThree[0]}, ${topThree[1]}, ${topThree[2]}. Total of ${total}`;
 
 }
 console.log(findElf(elfCalories))
