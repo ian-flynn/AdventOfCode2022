37bf6d78e995ad77820b334e251809bf0e20635b
Ian Flynn <ianflynnwork@gmail.com>
2023-01-16 20:08:50 -0500

    final cleanup

--- Day1/solution.js
+++ Day1/solution.js
@@ -1,30 +1,28 @@
 const fs = require('fs');
-const elfCalories = fs.readFileSync(`${__dirname}/input`).toString().split('\n')
+const calorieList = fs.readFileSync(`${__dirname}/input`).toString().split('\n');
 
-console.log(elfCalories);
-
-const findElf = (arr) => {
-    let bigCals = 0, sum = 0;
-    const topThree = [];
-    arr.forEach(cal => {
-        if(!cal){
-            //check if top three is has less than 3 things in it, if so add sum to it
-            if(topThree.length < 3) topThree.push(sum)
-            if(topThree.length === 3 && sum > Math.min(...topThree)){
-                topThree.sort((a,b) => a-b).shift()
-                topThree.push(sum)
-            }
-            sum = 0;
-
-        } else {
-            sum+= parseInt(cal)
-            if(sum > bigCals){
-                bigCals = sum;
-            }
+let bigCals = 0, sum = 0;
+const topThree = [];
+calorieList.forEach(cal => {
+    if(!cal){
+        if(topThree.length < 3 || sum > Math.min(...topThree)) {
+            topThree.push(sum);
+            topThree.sort((a,b)=>a-b).reverse();
         }
-    })
-    let total = topThree.reduce((acc,curr)=> acc+curr)
-    return `The elf carring the most has ${bigCals} calories, the top three are ${topThree[0]}, ${topThree[1]}, ${topThree[2]}. Total of ${total}`;
-
+        if(topThree.length > 3) topThree.pop();
+        sum = 0;
+    } else {
+        sum+= parseInt(cal);
+        if(sum > bigCals){
+            bigCals = sum;
+        }
+    }
+});
+if(sum > Math.min(...topThree)){
+    topThree.push(sum);
+    topThree.sort((a,b)=>a-b).reverse();
+    topThree.pop();
 }
-console.log(findElf(elfCalories))
+
+const total = topThree.reduce((acc,curr)=> acc+curr);
+console.log(`The elf carring the most has ${bigCals} calories, the top three have ${topThree[0]}, ${topThree[1]}, ${topThree[2]}. Total of ${total}`);
