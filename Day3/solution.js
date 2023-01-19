ea89a9c5aa66408b84fc570e1074c048f178b040
Ian Flynn <ianflynnwork@gmail.com>
2023-01-19 12:56:27 -0500

    s

--- Day3/solution.js
+++ Day3/solution.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
 const folderPath = '/Users/ianflynn/Documents/GitHub/AdventOfCode2022/inputsSubmodule/Day3';
 const info = fs.readFileSync(folderPath).toString().split('\n')
-console.log(info)
+// console.log(info)
+//'zBBtHnnHtwwHplmlRlzPLCpp'
+info.forEach(el => {
+    const firstHalf = ;
+    const secondHalf = ;
+})
