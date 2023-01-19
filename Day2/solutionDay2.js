736394596cd83af5b51ed08afa195865643d5072
Ian Flynn <ianflynnwork@gmail.com>
2023-01-18 20:58:57 -0500

    s

--- Day2/solutionDay2.js
+++ Day2/solutionDay2.js
@@ -11,26 +11,26 @@ const moves = fs.readFileSync(`${__dirname}/input`).toString().split('\n');
 //X beats C loses to B
 //Y beats A loses to C
 //Z beats B loses to A
-
+const points = {A: 1, B:2, C:3, win:6, draw:3};
 let score = 0
 moves.forEach(el => {
     if(el[0] == 'A'){//Rock
-        if(el[2] == 'X'){score += 4}//Rock tie
-        if(el[2] == 'Y'){score += 8}//Paper win
-        if(el[2] == 'Z'){score += 3}//Scissors lose
+        if(el[2] == 'X'){score += points.draw + points.A}//Rock tie
+        if(el[2] == 'Y'){score += points.win + points.B}//Paper win
+        if(el[2] == 'Z'){score += points.C}//Scissors lose
     }
     if(el[0] == 'B'){//Paper
-        if(el[2] == 'X'){score += 1}//Rock lose
-        if(el[2] == 'Y'){score += 5}//Paper tie
-        if(el[2] == 'Z'){score += 9}//Scissors win
+        if(el[2] == 'X'){score += points.A}//Rock lose
+        if(el[2] == 'Y'){score += points.draw + points.B}//Paper tie
+        if(el[2] == 'Z'){score += points.win + points.C}//Scissors win
     }
     if(el[0] == 'C'){//Scissors
-        if(el[2] == 'X'){score += 7}//Rock win
-        if(el[2] == 'Y'){score += 2}//Paper lose
-        if(el[2] == 'Z'){score += 6}//Scissors tie
+        if(el[2] == 'X'){score += points.win + points.A}//Rock win
+        if(el[2] == 'Y'){score += points.B}//Paper lose
+        if(el[2] == 'Z'){score += points.draw + points.C}//Scissors tie
     }
 })
-// console.log(score)
+console.log(score)
 
 // PART TWO
 
@@ -48,19 +48,19 @@ moves.forEach(el => {
 let newScore = 0
 moves.forEach(el => {
     if(el[2] == 'X'){//Lose
-        if(el[0] == 'A'){newScore += 3}//Rock , Scissor
-        if(el[0] == 'B'){newScore += 1}//Paper, Rock
-        if(el[0] == 'C'){newScore += 2}//Scissors, Paper
+        if(el[0] == 'A'){newScore += points.C}//Rock , Scissor
+        if(el[0] == 'B'){newScore += points.A}//Paper, Rock
+        if(el[0] == 'C'){newScore += points.B}//Scissors, Paper
     }
     if(el[2] == 'Y'){//Draw
-        if(el[0] == 'A'){newScore += 4}//Rock 
-        if(el[0] == 'B'){newScore += 5}//Paper
-        if(el[0] == 'C'){newScore += 6}//Scissors
+        if(el[0] == 'A'){newScore += points.A + points.draw}//Rock 
+        if(el[0] == 'B'){newScore += points.B + points.draw}//Paper
+        if(el[0] == 'C'){newScore += points.C + points.draw}//Scissors
     }
     if(el[2] == 'Z'){//Win
-        if(el[0] == 'A'){newScore += 8}//Rock 
-        if(el[0] == 'B'){newScore += 9}//Paper
-        if(el[0] == 'C'){newScore += 7}//Scissors
+        if(el[0] == 'A'){newScore += points.B + points.win}//Rock 
+        if(el[0] == 'B'){newScore += points.C + points.win}//Paper
+        if(el[0] == 'C'){newScore += points.A + points.win}//Scissors
     }
 })
 console.log(newScore)
