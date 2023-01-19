5ab5f9faf2b091a17b9e79beda107d11d53eb7d6
Ian Flynn <ianflynnwork@gmail.com>
2023-01-18 20:44:03 -0500

    part 2 solve

--- Day2/solutionDay2.js
+++ Day2/solutionDay2.js
@@ -30,4 +30,38 @@ moves.forEach(el => {
         if(el[2] == 'Z'){score += 6}//Scissors tie
     }
 })
-console.log(score)
+// console.log(score)
+
+// PART TWO
+
+//A Rock      1
+//B Paper     2
+//C Scissors  3
+//plus
+//Lose          0
+//Tie           3
+//Win           6
+//X need to Lose
+//Y need to Draw
+//Z need to Win
+
+let newScore = 0
+moves.forEach(el => {
+    if(el[2] == 'X'){//Lose
+        if(el[0] == 'A'){newScore += 3}//Rock , Scissor
+        if(el[0] == 'B'){newScore += 1}//Paper, Rock
+        if(el[0] == 'C'){newScore += 2}//Scissors, Paper
+    }
+    if(el[2] == 'Y'){//Draw
+        if(el[0] == 'A'){newScore += 4}//Rock 
+        if(el[0] == 'B'){newScore += 5}//Paper
+        if(el[0] == 'C'){newScore += 6}//Scissors
+    }
+    if(el[2] == 'Z'){//Win
+        if(el[0] == 'A'){newScore += 8}//Rock 
+        if(el[0] == 'B'){newScore += 9}//Paper
+        if(el[0] == 'C'){newScore += 7}//Scissors
+    }
+})
+console.log(newScore)
+
