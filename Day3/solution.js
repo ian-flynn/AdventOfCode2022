49642abafd7f95d863b5a19a13361051b1685839
Ian Flynn <ianflynnwork@gmail.com>
2023-01-24 23:22:07 -0500

    solution complete

--- Day3/solution.js
+++ Day3/solution.js
@@ -2,8 +2,12 @@ const fs = require('fs');
 const folderPath = '/Users/ianflynn/Documents/GitHub/AdventOfCode2022/inputsSubmodule/Day3';
 const info = fs.readFileSync(folderPath).toString().split('\n')
 // console.log(info)
-//'zBBtHnnHtwwHplmlRlzPLCpp'
+let total = 0;
+const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 info.forEach(el => {
-    // const firstHalf = ;
-    // const secondHalf = ;
+    const firstCompartment = el.slice(0, el.length/2).split('');
+    const secondCompartment = el.slice(el.length/2);
+    const match = firstCompartment.filter(el => secondCompartment.includes(el))
+    total += alphabet.indexOf(...match)+1
 })
+console.log(total)
