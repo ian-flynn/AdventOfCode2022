c41572d3189dc0ec76014677453fb1656a5af0fb
Ian Flynn <ianflynnwork@gmail.com>
2023-02-07 10:59:41 -0500

    part one final cleanup

--- Day4/solution.js
+++ Day4/solution.js
@@ -1,34 +1,26 @@
 const fs = require('fs');
 const folderPath = '../Inputs/Day4';
 const cleaningPairs = fs.readFileSync(folderPath).toString().split('\n')
+// console.log(cleaningPairs)
 
 // == Part One ==
 
-function pairFormatter(array){
+function pairFormatter(array){ // '1-69,37-69' ==> [ [1, 69], [37, 69] ]
     return array.map(pair => {
         return pair.split(',').map(elem => elem.split('-').map(string => parseInt(string)))
     })
 }
 const formattedPairs = pairFormatter(cleaningPairs)
+console.log(formattedPairs)
 
 function overlapDetector(array){
-    // let overlaps = 0;
-    // array.forEach(pair => { 
-    //     const elfOne = pair[0]
-    //     const elfTwo = pair[1]
-    //     if(elfOne[0] <= elfTwo[0] && elfOne[1] >= elfTwo[1]) overlaps++
-    //     else if(elfTwo[0] <= elfOne[0] && elfTwo[1] >= elfOne[1]) overlaps++
-    // })
-    // return overlaps
-    
-   return array.reduce((acc, curr) => { 
-        const elfOne = curr[0]
-        const elfTwo = curr[1]
-        if(elfOne[0] <= elfTwo[0] && elfOne[1] >= elfTwo[1]) acc++
-        else if(elfTwo[0] <= elfOne[0] && elfTwo[1] >= elfOne[1]) acc++
+    return array.reduce((acc, curr) => { //  [ [1, 69], [37, 69] ]
+        const elfOne = curr[0] //  [1, 69]
+        const elfTwo = curr[1] //  [37, 69]
+        if(elfOne[0] <= elfTwo[0] && elfOne[1] >= elfTwo[1]) return acc + 1
+        if(elfTwo[0] <= elfOne[0] && elfTwo[1] >= elfOne[1]) return acc + 1
         return acc;
     }, 0)
-    // return overlaps
 }
 
 const overlapsFound = overlapDetector(formattedPairs)
