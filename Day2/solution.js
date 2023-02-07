60907530ab5c89af345b81ef4a8ba0f901a27455
Ian Flynn <ianflynnwork@gmail.com>
2023-02-06 20:51:28 -0500

    changed day2 solution for the better

--- Day2/solution.js
+++ Day2/solution.js
@@ -2,37 +2,28 @@ const fs = require('fs');
 const folderPath = '../Inputs/Day2';
 const moves = fs.readFileSync(folderPath).toString().split('\n')
 
-// console.log(moves)
-//A X Rock      1
-//B Y Paper     2
-//C Z Scissors  3
-//plus
-//Lose          0
-//Tie           3
-//Win           6
-//X beats C loses to B
-//Y beats A loses to C
-//Z beats B loses to A
+// == PART ONE ==
+
+function findTotalPointsPlan (array){
+    const newMoves = 'ABCXYZ';
+    let score = 0, win = 6, tie = 3, lose = 0;
+    const rps = { 1: 'rock', 2: 'paper', 3: 'scissors' }
+    array.forEach(play => {
+        const theirPlay = newMoves.indexOf(play[0]) % 3 + 1;
+        const myPlay = newMoves.indexOf(play[2]) % 3 + 1;
+        if(theirPlay === myPlay) return score += tie + myPlay
+        if(rps[theirPlay] === 'rock' && rps[myPlay] === 'scissors') return score += lose + myPlay
+        if(rps[theirPlay] === 'paper' && rps[myPlay] === 'rock') return score += lose + myPlay
+        if(rps[theirPlay] === 'scissors' && rps[myPlay] === 'paper') return score += lose + myPlay
+        score += win + myPlay
+    })
+    return score
+}
+console.log('Part One Answer: ', findTotalPointsPlan(moves))
+
+
+// the index remainder 3 is the move 
 const points = {A: 1, B:2, C:3, win:6, draw:3};
-let score = 0
-moves.forEach(el => {
-    if(el[0] == 'A'){//Rock
-        if(el[2] == 'X'){score += points.draw + points.A}//Rock tie
-        if(el[2] == 'Y'){score += points.win + points.B}//Paper win
-        if(el[2] == 'Z'){score += points.C}//Scissors lose
-    }
-    if(el[0] == 'B'){//Paper
-        if(el[2] == 'X'){score += points.A}//Rock lose
-        if(el[2] == 'Y'){score += points.draw + points.B}//Paper tie
-        if(el[2] == 'Z'){score += points.win + points.C}//Scissors win
-    }
-    if(el[0] == 'C'){//Scissors
-        if(el[2] == 'X'){score += points.win + points.A}//Rock win
-        if(el[2] == 'Y'){score += points.B}//Paper lose
-        if(el[2] == 'Z'){score += points.draw + points.C}//Scissors tie
-    }
-})
-console.log(score)
 
 // PART TWO
 
@@ -65,5 +56,5 @@ moves.forEach(el => {
         if(el[0] == 'C'){newScore += points.A + points.win}//Scissors
     }
 })
-console.log(newScore)
+console.log('part two ', newScore)
 
