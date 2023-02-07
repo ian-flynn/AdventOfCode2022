27be3fe82fd6029b8f52561596a11546e3a68e3d
Ian Flynn <ianflynnwork@gmail.com>
2023-02-06 21:36:07 -0500

    day2 solution part 1 and 2 lookin good.

--- Day2/solution.js
+++ Day2/solution.js
@@ -3,58 +3,47 @@ const folderPath = '../Inputs/Day2';
 const moves = fs.readFileSync(folderPath).toString().split('\n')
 
 // == PART ONE ==
+const points = {rock: 1, paper: 2, scissors: 3, win: 6, tie: 3, lose: 0};
 
 function findTotalPointsPlan (array){
-    const newMoves = 'ABCXYZ';
-    let score = 0, win = 6, tie = 3, lose = 0;
-    const rps = { 1: 'rock', 2: 'paper', 3: 'scissors' }
+    let score = 0;
+    const moves = { A: 'rock', B: 'paper', C: 'scissors', X: 'rock', Y: 'paper', Z: 'scissors'};
+    
     array.forEach(play => {
-        const theirPlay = newMoves.indexOf(play[0]) % 3 + 1;
-        const myPlay = newMoves.indexOf(play[2]) % 3 + 1;
-        if(theirPlay === myPlay) return score += tie + myPlay
-        if(rps[theirPlay] === 'rock' && rps[myPlay] === 'scissors') return score += lose + myPlay
-        if(rps[theirPlay] === 'paper' && rps[myPlay] === 'rock') return score += lose + myPlay
-        if(rps[theirPlay] === 'scissors' && rps[myPlay] === 'paper') return score += lose + myPlay
-        score += win + myPlay
+        const theirPlay = moves[play[0]]
+        const myPlay = moves[play[2]]
+        if(theirPlay === myPlay) return score += points.tie + points[myPlay]
+        if(theirPlay === 'rock' && myPlay === 'scissors') return score += points.lose + points[myPlay]
+        if(theirPlay === 'paper' && myPlay === 'rock') return score += points.lose + points[myPlay]
+        if(theirPlay === 'scissors' && myPlay === 'paper') return score += points.lose + points[myPlay]
+        score += points.win + points[myPlay]
     })
     return score
 }
-console.log('Part One Answer: ', findTotalPointsPlan(moves))
+const partOnePlan = findTotalPointsPlan(moves) // ==> 13268
+console.log('Part One Answer: ', partOnePlan)
 
+// == PART TWO ==
 
-// the index remainder 3 is the move 
-const points = {A: 1, B:2, C:3, win:6, draw:3};
-
-// PART TWO
-
-//A Rock      1
-//B Paper     2
-//C Scissors  3
-//plus
-//Lose          0
-//Tie           3
-//Win           6
-//X need to Lose
-//Y need to Draw
-//Z need to Win
-
-let newScore = 0
-moves.forEach(el => {
-    if(el[2] == 'X'){//Lose
-        if(el[0] == 'A'){newScore += points.C}//Rock , Scissor
-        if(el[0] == 'B'){newScore += points.A}//Paper, Rock
-        if(el[0] == 'C'){newScore += points.B}//Scissors, Paper
-    }
-    if(el[2] == 'Y'){//Draw
-        if(el[0] == 'A'){newScore += points.A + points.draw}//Rock 
-        if(el[0] == 'B'){newScore += points.B + points.draw}//Paper
-        if(el[0] == 'C'){newScore += points.C + points.draw}//Scissors
-    }
-    if(el[2] == 'Z'){//Win
-        if(el[0] == 'A'){newScore += points.B + points.win}//Rock 
-        if(el[0] == 'B'){newScore += points.C + points.win}//Paper
-        if(el[0] == 'C'){newScore += points.A + points.win}//Scissors
-    }
-})
-console.log('part two ', newScore)
+function updatedPlan(array){
+    let score = 0
+    const moves = { A: 'rock', B: 'paper', C: 'scissors', X: 'lose', Y: 'tie', Z: 'win'};
+    
+    array.forEach(play => {
+        const theirPlay = moves[play[0]]
+        const myPlay = moves[play[2]]
+        if(myPlay === 'tie') return score += points.tie + points[theirPlay]
+        if(myPlay === 'lose'){
+            if(theirPlay === 'rock') return score += points.lose + points.scissors
+            if(theirPlay === 'paper') return score += points.lose + points.rock
+            return score += points.lose + points.paper
+        }// else I must win
+        if(theirPlay === 'rock') return score += points.win + points.paper
+        if(theirPlay === 'paper') return score += points.win + points.scissors
+        return score += points.win + points.rock
+    })
+    return score;
+}
+const newMoveTotal = updatedPlan(moves)
+console.log('Part Two Solution: ', newMoveTotal) // ==> 15508
 
