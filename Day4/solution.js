7353b66012eb0ec0556a6cf46f98fd0dc7f08635
Ian Flynn <ianflynnwork@gmail.com>
2023-02-07 10:54:05 -0500

    two solutions for part1

--- Day4/solution.js
+++ Day4/solution.js
@@ -1,22 +1,37 @@
 const fs = require('fs');
 const folderPath = '../Inputs/Day4';
-let cleaningPairs = fs.readFileSync(folderPath).toString().split('\n')
-// console.log(cleaningPairs)
+const cleaningPairs = fs.readFileSync(folderPath).toString().split('\n')
 
- cleaningPairs = cleaningPairs.map(pair => {
-    return pair.split(',').map(elem => elem.split('-').map(string => parseInt(string))) //.flat()
-})
-// console.log('new', test)
-let overlapsFound = 0;
-cleaningPairs.forEach(pair => { // [ [ 5, 90 ], [ 4, 90 ] ]
-    const elfOne = pair[0]
-    const elfTwo = pair[1]
-    if(elfOne[0] <= elfTwo[0] && elfOne[1] >= elfTwo[1]){
-        overlapsFound++
-        console.log(pair, 'one is enveloping two')
-    } else if(elfTwo[0] <= elfOne[0] && elfTwo[1] >= elfOne[1]) {
-        console.log(pair, 'two is enveloping one')
-        overlapsFound++
-    }
-})
-console.log('final score', overlapsFound) // ==> 536
+// == Part One ==
+
+function pairFormatter(array){
+    return array.map(pair => {
+        return pair.split(',').map(elem => elem.split('-').map(string => parseInt(string)))
+    })
+}
+const formattedPairs = pairFormatter(cleaningPairs)
+
+function overlapDetector(array){
+    // let overlaps = 0;
+    // array.forEach(pair => { 
+    //     const elfOne = pair[0]
+    //     const elfTwo = pair[1]
+    //     if(elfOne[0] <= elfTwo[0] && elfOne[1] >= elfTwo[1]) overlaps++
+    //     else if(elfTwo[0] <= elfOne[0] && elfTwo[1] >= elfOne[1]) overlaps++
+    // })
+    // return overlaps
+    
+   return array.reduce((acc, curr) => { 
+        const elfOne = curr[0]
+        const elfTwo = curr[1]
+        if(elfOne[0] <= elfTwo[0] && elfOne[1] >= elfTwo[1]) acc++
+        else if(elfTwo[0] <= elfOne[0] && elfTwo[1] >= elfOne[1]) acc++
+        return acc;
+    }, 0)
+    // return overlaps
+}
+
+const overlapsFound = overlapDetector(formattedPairs)
+console.log('Part One Solution: ', overlapsFound) // ==> 536
+
+// == Part Two ==
