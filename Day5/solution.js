0680481c63bd886f56df49d988c83162b8904e50
Ian Flynn <ianflynnwork@gmail.com>
2023-02-08 17:42:46 -0500

    day5 final cleanup

--- Day5/solution.js
+++ Day5/solution.js
@@ -4,7 +4,6 @@ const splitInfo = fs.readFileSync(folderPath).toString().split('\n\n')
 
 const stacksInfo = splitInfo[0].split('\n').map(row => row.split('')).slice(0, -1)
 const movesInfo = splitInfo[1].split('\n')
-console.log('stacksInfo: ', stacksInfo)
 
 function stackMaker(inputArray) {
     const array = JSON.parse(JSON.stringify(inputArray)); // Deep copy so I can splice off values
@@ -22,17 +21,7 @@ function stackMaker(inputArray) {
     }
     return stacks.map(stack => stack.map(item => item[1]).reverse())
 }
-//0 [ 'L', 'N', 'W', 'T', 'D' ]
-//1 [ 'C', 'P', 'H' ]
-//2 [ 'W', 'P', 'H', 'N', 'D', 'G', 'M', 'J' ]
-//3 [ 'C', 'W', 'S', 'N', 'T', 'Q','L' ]
-//4 [ 'P', 'H', 'C', 'N' ]
-//5 [ 'T', 'H', 'N', 'D', 'M', 'W', 'Q', 'B' ]
-//6 [ 'M', 'B', 'R', 'J', 'G', 'S', 'L' ]
-//7 [ 'Z', 'N', 'W', 'G', 'V', 'B', 'R', 'T' ]
-//8 [ 'W', 'G', 'D', 'N', 'P', 'L' ]
 
-//[ 'move', '6', 'from', '6', 'to', '5' ]
 function doMoves(craneModel) {
     const stacks = stackMaker(stacksInfo)
 
@@ -40,9 +29,9 @@ function doMoves(craneModel) {
         move = move.split(' ')
 
         let numberToMove = move[1]
-        const moveFrom = move[3] - 1 //index of actual stack are minus1 though so fix that
+        const moveFrom = move[3] - 1
         const moveTo = move[5] - 1
-        // console.log(numberToMove, moveFrom, moveTo)
+
         if (craneModel === 'CrateMover 9000') {
             while (numberToMove > 0) {
                 stacks[moveTo].push(stacks[moveFrom].pop())
@@ -50,29 +39,11 @@ function doMoves(craneModel) {
             }
         }
         if (craneModel === 'CrateMover 9001') {
-            //[ 'move', '6', 'from', '6', 'to', '5' ]
-            // const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-            // console.log(arr.splice(-6, 6))
-            // console.log(arr)
             stacks[moveTo].push(...stacks[moveFrom].splice(-numberToMove, numberToMove))
         }
-
     })
     return stacks
 }
-console.log('Part One Solution: ', doMoves('CrateMover 9000'))
-//0 [ 'T' ],
-//1 [ 'W' ],
-//2 [ 'W', 'J', 'N', 'S' ],
-//3 [ 'N', 'M', 'V', 'R','D', 'N', 'H', 'Z','T', 'P', 'J', 'G' ],
-//4 [ 'L', 'C', 'Q' ],
-//5 [ 'M', 'W', 'B','W', 'D', 'P','H'],
-//6 [ 'S', 'P', 'G', 'N', 'C','G', 'M', 'H', 'N', 'L','W', 'T', 'N', 'D', 'T','W', 'G', 'R', 'N'],
-//7 [ 'Q', 'D', 'H' ],
-//8 [ 'P', 'B', 'B', 'L', 'C', 'L' ]
-console.log('Part Two Solution: ', doMoves('CrateMover 9001'))
 
-//[ 'move', '6', 'from', '6', 'to', '5' ]
-// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-// console.log(arr.splice(-6, 6))
-// console.log(arr)
+console.log('Part One Solution: ', doMoves('CrateMover 9000').map(arr => arr.slice(-1)).flat().join(''))
+console.log('Part Two Solution: ', doMoves('CrateMover 9001').map(arr => arr.slice(-1)).flat().join(''))
